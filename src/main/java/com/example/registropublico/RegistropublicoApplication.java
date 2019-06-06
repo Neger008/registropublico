97cb9db66138da43f1a79f25e79f42eaa65b5cca
Neger008 <alfredo.full.cf@gmail.com>
2019-06-06 14:07:58 -0500

    1

---
+++ src/main/java/com/example/registropublico/RegistropublicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.registropublico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistropublicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistropublicoApplication.class, args);
+	}
+
+}
